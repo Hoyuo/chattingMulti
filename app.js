7d97362b9013f6d6366d1f006c35dcd192b27685
Hoyuo <psh9907@gmail.com>
2015-02-08 06:30:58 +0900

    rearrange

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var httpServer = http.createServer(app).listen(13000);
 var io = require('socket.io').listen(httpServer);
 
 var socket_ids = [];
+var count = 0;
 
 function registerUser(socket, nickname) {
     // socket_id와 nickname 테이블을 셋업
@@ -22,33 +23,43 @@ function registerUser(socket, nickname) {
 }
 
 io.sockets.on('connection', function (socket) {
-    console.log('connection', socket.id);
     socket.emit('new');
+
+    socket.on('UUID', function (data) {
+        var nickname = 'player' + count;
+        count++;
+        count = count % 2;
+        registerUser(socket, nickname);
+        console.log(nickname);
+    });
 
     socket.on('newSend', function (data) {
         registerUser(socket, data);
     });
 
     socket.on('btn', function (data) {
-        socket_id = socket_ids['webPage'];
-        if (socket_id != undefined) {
-            io.sockets.socket(socket_id).emit('broadcast_msg', {msg: data});
-        }// if
+        socket.get('nickname', function (err, nickname) {
+            var iValue = nickname.indexOf('0');
+            var socket_id = socket_ids['webPage'];
+            if (socket_id != undefined) {
+                if (iValue != -1)
+                    io.sockets.socket(socket_id).emit('broadcast_msg', {msg: data});
+                else
+                    io.sockets.socket(socket_id).emit('broadcast_msg_1', {msg: data});
+            }// if
+        });
     });
 
     socket.on('disconnect', function () {
         socket.get('nickname', function (err, nickname) {
             if (nickname != undefined) {
                 delete socket_ids[nickname];
-                io.sockets.emit('userlist', {users: Object.keys(socket_ids)});
-
-            }// if
+            }
         });
     });
 
     socket.on('send_msg', function (data) {
         socket.get('nickname', function (err, nickname) {
-
             data.msg = nickname + ' : ' + data.msg;
             if (data.to == 'ALL') socket.broadcast.emit('broadcast_msg', data); // 자신을 제외하고 다른 클라이언트에게 보냄
             else {
